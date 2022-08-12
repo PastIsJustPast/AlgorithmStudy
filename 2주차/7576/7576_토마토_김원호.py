381a55286fe1e8461d492e1790073d2cc6f98e57
Kim wonho <wonho.kim2@navercorp.com>
2022-08-12 16:53:18 +0900

    week2 upload

---
+++ 2주차/7576/7576_토마토_김원호.py
@@ -0,0 +1,33 @@
+from collections import deque
+
+M, N = map(int, input().split())
+box = [[int(x) for x in input().split()] for _ in range(N)]
+discover = [[0 for _ in range(M)] for _ in range(N)]
+riped_tomatos = []
+for i in range(N):
+    for j in range(M):
+        if box[i][j] == -1:
+            discover[i][j] = 1
+        elif box[i][j] == 1:
+            discover[i][j] = 1
+            riped_tomatos.append((i, j, 0))
+
+dx = [1, -1, 0, 0]
+dy = [0, 0, 1, -1]
+Q = deque(riped_tomatos)
+while Q:
+    x, y, day = Q.popleft()
+    for k in range(4):
+        nx = x + dx[k]
+        ny = y + dy[k]
+        if 0 <= nx < N and 0 <= ny < M:
+            if discover[nx][ny]:
+                continue
+            discover[nx][ny] = 1
+            Q.append((nx, ny, day+1))
+
+discover_cnt = sum(map(sum, discover))
+if discover_cnt != M * N:
+    print(-1)
+else:
+    print(day)
