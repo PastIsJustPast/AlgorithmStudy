5c466afe9d77117aaaf9d4db6f9437c08811e325
Jena <lunaliku@gmail.com>
2022-08-18 18:38:20 +0900

    update

--- 1주차/1106/1106호텔_김은우.py
+++ 1주차/1106/1106호텔_김은우.py
@@ -2,12 +2,10 @@ INF = 1000*20*100
 c, n = map(int, input().split())
 dp = [INF for _ in range(c+101)]
 dp[0] = 0
-info = []
 
-for _ in range(n):
-    cost, cust = map(int, input().split())  #비용, 고객
-    info.append((cost, cust))
+info = [tuple(map(int, input().split())) for _ in range(n)]
     
+#비용, 고객
 for cost, cust in info:
     for i in range(c+100):
         dp[i] = min(dp[i], dp[max(0, i-cust)]+cost)
