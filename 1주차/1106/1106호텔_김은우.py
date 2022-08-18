2d409070e8896a03a44740c6e87e7d524ca463d2
Jena <lunaliku@gmail.com>
2022-08-18 18:50:49 +0900

    update

--- 1주차/1106/1106호텔_김은우.py
+++ 1주차/1106/1106호텔_김은우.py
@@ -7,7 +7,7 @@ info = [tuple(map(int, input().split())) for _ in range(n)]
 
 #비용, 고객
 for cost, cust in info:
-    for i in range(c+100):
+    for i in range(c+1):
         dp[i] = min(dp[i], dp[max(0, i-cust)]+cost)
 
 print(dp[c])
