f9cece001f0a86657f35199a91b9f683ab05a7d2
Jena <lunaliku@gmail.com>
2022-09-01 18:46:27 +0900

    update

---
+++ 2주차/4090/4090_뱀파이어_숫자_김은우.py
@@ -0,0 +1,21 @@
+from itertools import permutations
+import sys
+input = sys.stdin.readline
+def search(x):
+    while True:
+        y = str(x)
+        l = len(y)
+
+        for perm in permutations(y, l):
+            if perm[0] == '0' :
+                continue
+            
+            num = int(''.join(perm))            
+            for i in range(1, l):
+                print(num%(10**i), num//(10**i), num%(10**i) * num//(10**i), x)
+                if (num%10**i) * (num//10**i) == x :
+                    return x
+        x += 1
+
+while (x := int(input())) != 0:
+    print(search(x))
